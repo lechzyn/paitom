1ed6791bec38f88fa626619560d214fd0c67c68c
João Gabriel <jotha77gabriel@gmail.com>
2025-09-17 04:15:59 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -11,46 +11,9 @@ def divisao(a,b):
     else:
         return a/b
 
-def menu():
-    while True:
-        print("1 - Soma")
-        print("2 - Subtracao")
-        print("3 - Multiplicacao")
-        print("4 - Divisao")
-        print("5 - Sair")
-
-        decisao = input("O que deseja realizar? ")
-        if (decisao == "5"):
-            print("Saindo do programa...")
-            break
-
-        numero1 = float(input("Digite o primeiro valor: "))
-        numero2 = float(input("Digite o segundo valor: "))
-
-        if(decisao == "1"):
-            resultado = soma(numero1, numero2)
-            print(resultado)
-            print("")
-        elif(decisao == "2"):
-            resultado = subtracao(numero1, numero2)
-            print(resultado)
-            print("")
-        elif(decisao == "3"):
-            resultado = multiplicacao(numero1, numero2)
-            print(resultado)
-            print("")
-        elif(decisao == "4"):
-            resultado = divisao(numero1, numero2)
-            print(resultado)
-            print("")
-
-        else:
-            print("Digito invalido!")
-
-
-
 if __name__ == "__main__":
     menu()
+
 
 
 
